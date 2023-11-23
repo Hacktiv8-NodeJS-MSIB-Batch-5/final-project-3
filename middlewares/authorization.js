ddfc13e23d66da215bed66a1ceb2cea553109566
eringnwn <eringnwn@gmail.com>
2023-11-23 17:00:00 +0700

    fix authorization

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,14 +1,14 @@
 const { User } = require("../models");
 
-const isAdmin = async(req, res) => {
+const isAdmin = async(req, res, next) => {
   const headerUserId = req.user_id;
   const user = await User.findByPk(headerUserId);
   if (user.role == "customer") {
     return res.status(401).json({
       message: "You are not authorized to view this content. Please log in as admin.",
     });
-    next();
   }
+  next();
 }
 
 module.exports = {
